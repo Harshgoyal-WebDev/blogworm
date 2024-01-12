c40e886d805420132609a643bd7fd0d76b9e3dcf
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-01-12 22:14:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="bg-slate-600 text-white w-full h-screen flex justify-center text-justify">yuss</h1>
+    </>
+  )
+}
+
+export default App
